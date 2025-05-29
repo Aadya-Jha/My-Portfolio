91847353198fd24e0399d1d57cc88f4ac0f759b3
Aadya-Jha <aadya.aarchi@gmail.com>
2025-05-29 20:17:53 +0530

    added links

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return(
         <div className="bg-black-dark min-h-screen w-20 fixed top-0 left-0 pl-10 pt-7 text-gray-300 ">
             <ul >
-                <li className="transform rotate-90 origin-left mt-8 font-medium text-xl font-inter hover:cursor-pointer hover:text-white 
-                hover:border-r-4 hover:border-purple">
-                    Skills
-                </li>
-                <li className="transform rotate-90 origin-left mt-16 font-medium text-xl font-inter hover:cursor-pointer">
-                    Projects
-                </li>
-                <li className="transform rotate-90 origin-left mt-24 font-medium text-xl font-inter hover:cursor-pointer"> 
-                    Experience
-                </li>
-                <li className="transform rotate-90 origin-left mt-32 font-medium text-xl font-inter hover:cursor-pointer">
-                    Education
-                </li>
+                <Link to="/skills">
+                    <li className="transform rotate-90 origin-left mt-8 font-medium text-xl font-inter hover:cursor-pointer hover:text-white 
+                        hover:border-r-4 hover:border-purple">
+                        Skills
+                    </li>
+                </Link>
+                <Link to="/projects">
+                    <li className="transform rotate-90 origin-left mt-16 font-medium text-xl font-inter hover:cursor-pointer">
+                        Projects
+                    </li>
+                </Link>
+                <Link to='/experience'>
+                    <li className="transform rotate-90 origin-left mt-24 font-medium text-xl font-inter hover:cursor-pointer"> 
+                        Experience
+                    </li>
+                </Link>
+                <Link to="/education">
+                    <li className="transform rotate-90 origin-left mt-32 font-medium text-xl font-inter hover:cursor-pointer">
+                        Education
+                    </li>
+                </Link>
             </ul>
         </div>
     );
--- portfolio/src/components/Topbar.jsx
+++ portfolio/src/components/Topbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import githubIcon from "../assets/github-white.png";
 import linkedinIcon from "../assets/linkedin-light.png";
 
@@ -25,9 +26,13 @@ const Topbar = () => {
                     </li>
                 </ul>
             </div>
-            <div className="ml-auto border-2 border-purple mr-36 mt-3 pl-6 pr-6 h-10 flex justify-center items-center rounded-xl hover:bg-purple group
-             hover:cursor-pointer">
-                <h2 className="text-purple font-semibold font-inter text-xl group-hover:text-white  ">RESUME</h2>
+            <div className="flex justify-center items-center ml-auto ">
+                <Link to="/resume">
+                    <div className="border-2 border-purple mr-36 mt-3 pl-6 pr-6 h-10 flex justify-center items-center rounded-xl hover:bg-purple group
+                    hover:cursor-pointer">
+                        <h2 className="text-purple font-semibold font-inter text-xl group-hover:text-white  ">Résumé</h2>
+                    </div>
+                </Link>
             </div>
         </div>
     );
